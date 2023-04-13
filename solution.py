9509776bd532b24cf8e695a437557363595827bc
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-13 20:56:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,9 @@ import scipy.stats as stat
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_value = stat.permutation_test((x, y), lambda x, y, \
-                 axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
-                 vectorized=True, 
-                 n_resamples=5000,
-                 alternative='greater').pvalue 
-    return p_value < 0.07
+#     p_value = stat.permutation_test((x, y), lambda x, y, \
+#                  axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+#                  vectorized=True, 
+#                  n_resamples=5000,
+#                  alternative='greater').pvalue 
+    return stat.ttest_ind(x, y)[1] < 0.07
